c927bf4c56bc5396466c39f53017ae2a55cac15a
aamrlyman <aamrlyman@gmail.com>
2022-08-29 19:46:30 -0700

    add CRUD functions to views and paths to project and app urls.py for CRUD functions

--- supers/serializers.py
+++ supers/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
 from .models import Super
 
-class SuperSerilaizer(serializers.ModelSerializer):
+class SuperSerializer(serializers.ModelSerializer):
     class Meta:
         model = Super
-        fields = ['id', 'name', 'alter_ego', 'primary_ability', 'catchphrase', 'super_type_id']
+        fields = ['id', 'name', 'alter_ego', 'primary_ability', 'secondary_ability', 'catchphrase', 'super_type', 'super_type_id']
         depth = 1
+
+    super_type_id = serializers.IntegerField(write_only = True)
--- supers/views.py
+++ supers/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response 
+from .models import Super
+from supers import serializers
+from .serializers import SuperSerializer
 
-# Create your views here.
+
+@api_view(['GET', 'POST'])
+def supers_list(request):
+    if request.method == 'GET':
+        super = Super.objects.all()
+        serializer = SuperSerializer(super, many=True)
+        return Response(serializer.data)
+    
+    elif request.method == "POST":
+        serializer = SuperSerializer(data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status= status.HTTP_201_CREATED)
+
+
+
+@api_view (["GET", "PUT", "DELETE"])
+def supers_detail(request, pk):
+    super = get_object_or_404(Super, pk=pk)
+    if request.method == "GET":
+        serializer = SuperSerializer(super);
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = SuperSerializer(super, data=request.data);
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    elif request.method == "DELETE":
+        super.delete()
+        return Response(status = status.HTTP_204_NO_CONTENT)
+# @api_view(["GET", "PUT", "DELETE"])
+# def product_detail(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     if request.method == 'GET':
+#         serializer = ProductSerializer(product);
+#         return Response(serializer.data)
+#     elif request.method == "PUT":
+#         serializer = ProductSerializer(product, data=request.data);
+#         serializer.is_valid(raise_exception=True)
+        # serializer.save()
+#         return Response(serializer.data)
+#     elif request.method == "DELETE":
+#              product.delete()
+#              return Response(status = status.HTTP_204_NO_CONTENT)
+
+
+
+
+
+
+
+# # @api_view(['GET', 'PUT', 'DELETE'])
+# # def product_detail(request, pk):
+# #     product = get_object_or_404 (Product, pk=pk)
+# #     if request.method == "GET":
+# #         serializer = ProductSerializer(Product);
+# #         return Response(serializer.data)
+# #     elif request.method == "PUT":
+# #         serializer = ProductSerializer(Product, data = request.data);
+# #         serializer.is_valid(raise_exception=True)
+# #         serializer.save()
+# #         return Response(serializer.data) 
